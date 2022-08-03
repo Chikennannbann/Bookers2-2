13a31aee445e96a10a5e66137b1d9795e708db58
Misaki Kawamura <102850169+Chikennannbann@users.noreply.github.com>
2022-08-03 13:27:48 +0000

    [add] header, view/books, homes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,46 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @booknew = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @booknew = Book.new
+  end
+
+  def create
+    @booknew = Book.new(book_params)
+    @booknew.user_id = current_user.id
+    if @booknew.save
+      redirect_to book_path(@booknew), notice: "You have created a book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "The book has been updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "You have deleted the book successfully."
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
