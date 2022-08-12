e99783709ac931034f26de59261c900c16c759d3
Misaki Kawamura <102850169+Chikennannbann@users.noreply.github.com>
2022-08-12 14:21:26 +0000

    [add] relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'relationships/index'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
